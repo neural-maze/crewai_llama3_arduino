6dddc69dbe03d851281087356597ed05045bdd31
MichaelisTrofficus <miguel.otero.pedrido.1993@gmail.com>
2024-05-11 20:14:45 +0200

    create requirements file

--- tmp/tmp.ino
+++ tmp/tmp.ino
@@ -1,20 +1,13 @@
 void setup() {
-  pinMode(9, OUTPUT); // Set pin 9 as output
-  pinMode(10, OUTPUT); // Set pin 10 as output
-  pinMode(11, OUTPUT); // Set pin 11 as output
 }
 
 void loop() {
-  digitalWrite(9, HIGH); // Turn on the green LED
+  digitalWrite(9, HIGH);
   delay(1000);
   
-  digitalWrite(9, LOW);
-  digitalWrite(10, HIGH); // Turn on the blue LED
+  digitalWrite(11, HIGH);
   delay(1000);
   
-  digitalWrite(10, LOW);
-  digitalWrite(11, HIGH); // Turn on the red LED
+  digitalWrite(10, HIGH);
   delay(1000);
-
-  digitalWrite(11, LOW);
 }
